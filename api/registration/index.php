d9ba4b83e47ceaa4721f9c9442ae9195775e8d89
kubap545 <kubap454@interia.pl>
2025-01-11 16:51:54 +0100

    Rejestracja v2

--- api/registration/index.php
+++ api/registration/index.php
@@ -3,29 +3,44 @@ session_start();
 if (isset($_POST['action'])) {
     require(__DIR__ . '/../functions/db-connect.php');
     $login = $_POST['login'];
-    $password = $_POST['password'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $email = $_POST['email'];
-    isset($_POST['name']) ? $name =  $_POST['name'] : $name = '';
-    isset($_POST['surname']) ? $surname =  $_POST['surname'] : $surname = '';
+    $name = $_POST['name'] ?? '';
+    $surname = $_POST['surname'] ?? '';
 
-    $sql = "INSERT INTO users (`id`, `name`, `surname`, `login`, `password`, `email`, `accountIsActive`, `lastLoggin`, `editDate`) VALUES (`NULL`,`?`,`?`,`?`,`?`,`?`,`0`,`0000-00-00 00:00:00`,`0000-00-00 00:00:00`);";
+    $sql = "SELECT 1 FROM users WHERE login = ?;";
     $stmt = $GLOBALS['link']->prepare($sql);
-    $stmt->bind_param('si', $login, $id);
+    $stmt->bind_param('s', $login);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if ($result->num_rows === 1) {
-        $row = $result->fetch_assoc();
-        if (password_verify($password, $row['password'])) {
-            // $_SESSION['ma']
+    if ($result && $result->num_rows > 0) {
+        $_SESSION['regError'] = 'Podany login już istnieje!';
+        header("Location: /registration/");
+    }
 
-            header('Location: /login/');
-        } else {
-            $_SESSION['loginError'] = 'Nieprawidłowe hasło';
-            header('Location: /login/');
-        }
+    $sql = "SELECT 1 FROM users WHERE email = ?;";
+    $stmt = $GLOBALS['link']->prepare($sql);
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result && $result->num_rows > 0) {
+        $_SESSION['regError'] = 'Podany e-mail już istnieje!';
+        header("Location: /registration/");
+    }
+
+    $verifyCode = substr(str_shuffle(str_repeat($x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(20 / strlen($x)))), 1, 20);
+
+    $sql = "INSERT INTO users (id, name, surname, login, password, email, accountIsActive, lastLoggin, editDate) VALUES (NULL,?,?,?,?,?,'$verifyCode','0000-00-00 00:00:00','0000-00-00 00:00:00');";
+    $stmt = $GLOBALS['link']->prepare($sql);
+    $stmt->bind_param('sssss', $name, $surname, $login, $password, $email);
+    $stmt->execute();
+
+    if ($stmt->execute()) {
+        header('Location: /registration/?reg=success');
     } else {
-        $_SESSION['loginError'] = 'Nieprawidłowy login';
-        header('Location: /login/');
+        $_SESSION['regError'] = 'Wystąpił błąd podczas rejestracji. Spróbuj ponownie.';
+        header('Location: /registration/');
     }
 }
--- registration/index.php
+++ registration/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 ?>
 
 
@@ -13,41 +14,67 @@
 
 <body>
     <div>
-        <form action="/api/registration/" method="POST">
-            <div>
-                <div class="input-box">
-                    <label for="login">Login:</label>
-                    <input type="text" name="login" id="login" required>
+        <?php
+
+        if (isset($_GET['reg']) && $_GET['reg'] == 'success')
+            echo "
+             <h1>Dziękujemy za rejestrację!</h1>
+        <p>Twoje konto zostało pomyślnie utworzone.</p>
+        <p>Wysłaliśmy do Ciebie e-mail z linkiem aktywacyjnym, który należy kliknąć, aby zakończyć proces rejestracji.</p>
+        <p><strong>Co dalej?</strong></p>
+        <ul style='text-align: left; margin: 0 auto; padding-left: 20px;'>
+            <li>Sprawdź swoją skrzynkę pocztową (w tym folder Spam, jeśli wiadomość nie jest widoczna w folderze głównym).</li>
+            <li>Kliknij w link aktywacyjny w wiadomości, aby aktywować swoje konto.</li>
+        </ul>
+        <p>Jeśli nie otrzymałeś e-maila w ciągu kilku minut, skontaktuj się z nami, abyśmy mogli Ci pomóc.</p>
+        <p>Cieszymy się, że dołączyłeś do naszej społeczności!</p>
+        <a href='/login/'>Przejdź do logowania!</a>";
+        else {
+            if (isset($_SESSION['regError'])) {
+                echo $_SESSION['regError'];
+                unset($_SESSION['regError']);
+            }
+
+        ?>
+            <form action="/api/registration/" method="POST">
+                <div>
+                    <div class="input-box">
+                        <label for="login">Login:</label>
+                        <input type="text" name="login" id="login" required>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="input-box">
-                    <label for="password">Hasło:</label>
-                    <input type="password" name="password" id="password" required>
+                <div>
+                    <div class="input-box">
+                        <label for="password">Hasło:</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="input-box">
-                    <label for="name">Imię:</label>
-                    <input type="text" name="name" id="name">
+                <div>
+                    <div class="input-box">
+                        <label for="name">Imię:</label>
+                        <input type="text" name="name" id="name">
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="input-box">
-                    <label for="surname">Nazwisko:</label>
-                    <input type="text" name="surname" id="surname">
+                <div>
+                    <div class="input-box">
+                        <label for="surname">Nazwisko:</label>
+                        <input type="text" name="surname" id="surname">
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="input-box">
-                    <label for="email">E-mail:</label>
-                    <input type="email" name="email" id="email" required>
+                <div>
+                    <div class="input-box">
+                        <label for="email">E-mail:</label>
+                        <input type="email" name="email" id="email" required>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <button type="submit">Zarejestruj się</button>
-            </div>
-        </form>
+                <div>
+                    <input type="hidden" name="action">
+                    <button type="submit">Zarejestruj się</button>
+                </div>
+            </form>
+        <?php
+        }
+        ?>
     </div>
 
 </body>
